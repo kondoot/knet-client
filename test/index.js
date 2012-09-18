ebc8790920d6d7d6fb351e80d8f6d8d3507e3a3f
Coen Hyde <coen.hyde@gmail.com>
2012-09-18 13:59:40 +1000

    working implementation

---
+++ test/index.js
@@ -0,0 +1,42 @@
+var should = require('should')
+  , nock = require('nock');
+
+var SkynetClient = require('../lib/skynet-client');
+  
+var testOptions = {
+  host: 'http://skynet-test.kondoot.com.au'
+, username: 'kondoot'
+, password: 'woohoo'
+};
+
+var skynetHostMock = nock(testOptions.host).matchHeader('Authorization', 'Basic a29uZG9vdDp3b29ob28=');
+
+var skynet = new SkynetClient(testOptions);
+
+describe('Skynet Client Library', function(){
+  it('A GET request', function(done) {
+    skynetHostMock.get('/roles').reply(200, [
+      { name: 'app', package: 'Small 1GB', dataset: 'smartos'}
+    , { name: 'lb', package: 'Small 1GB', dataset: 'smartos'}
+    ]);
+    
+    skynet.listRoles(function(err, res, roles) {
+      should.not.exist(err);
+      roles.should.have.length(2);
+      roles[0].name.should.equal('app');
+      done();
+    });
+  });
+  it('A POST request', function(done) {
+    var data = { name: 'app', package: 'Small 1GB', dataset: 'smartos'};
+    skynetHostMock.post('/role/app', data).reply(200, function(uri, requestBody) {
+      return requestBody;
+    });
+    
+    skynet.upsertRole(data.name, data, function(err, res, role) {
+      should.not.exist(err);
+      role.should.eql(data);
+      done();
+    });
+  });
+});
